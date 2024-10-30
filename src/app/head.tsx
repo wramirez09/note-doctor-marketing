60d75b80290fdffbf71093890896f2c12777da18
Johnathan Guzman <johnathanthedev@gmail.com>
2024-10-30 13:28:06 -0500

    (Util) Add favicon.ico

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>NoteDoctor.ai</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="" />
-      <link rel="icon" href="/images/favicon.png" />
+      <link rel="icon" href="/favicon.ico" />
     </>
   );
 }
