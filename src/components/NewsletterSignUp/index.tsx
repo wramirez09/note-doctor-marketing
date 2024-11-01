4687b2da70fc334b7c759ef52df57929b8525e1a
wramirez09 <wramirez1980@gmail.com>
2024-11-01 00:31:50 -0500

    fix spacing issue

--- src/components/NewsletterSignUp/index.tsx
+++ src/components/NewsletterSignUp/index.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import { Button, TextInput } from "@mantine/core";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import SectionTitle from "../Common/SectionTitle";
 import classes from "./index.module.css";
 
@@ -18,35 +18,38 @@ export const NewsletterSignUp: React.FC<{
   width = "680px",
   mt = "120px",
 }) => {
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 1.5 }}
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    >
+      <section
+        className={`relative z-20 overflow-hidden bg-white pb-8  dark:bg-dark lg:pb-[50px] pt-[${mt}]`}
       >
-        <section
-          className={`relative z-20 overflow-hidden bg-white pb-8  dark:bg-dark lg:pb-[50px] lg:pt-[${mt}]`}
-        >
-          <div className="container md:px-12">
-            <SectionTitle
-              title={title}
-              paragraph={paragraph}
-              width={width}
-              center
-            />
-            <div className="flex justify-center">
-              <div className={classes.controls}>
-                <TextInput
-                  placeholder="Your email"
-                  classNames={{ input: classes.input, root: classes.inputWrapper }}
-                />
-                <Button className={`${classes.control} button-primary`}>
-                  {btnText}
-                </Button>
-              </div>
+        <div className="container md:px-12">
+          <SectionTitle
+            title={title}
+            paragraph={paragraph}
+            width={width}
+            center
+          />
+          <div className="flex justify-center">
+            <div className={classes.controls}>
+              <TextInput
+                placeholder="Your email"
+                classNames={{
+                  input: classes.input,
+                  root: classes.inputWrapper,
+                }}
+              />
+              <Button className={`${classes.control} button-primary`}>
+                {btnText}
+              </Button>
             </div>
           </div>
-        </section>
-      </motion.div>
-    );
-  };
+        </div>
+      </section>
+    </motion.div>
+  );
+};
