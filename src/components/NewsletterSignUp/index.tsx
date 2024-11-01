55ac1efd5ff8825db6908a7a29854156cfe13e08
wramirez09 <wramirez1980@gmail.com>
2024-10-31 22:01:12 -0500

    revert config

--- src/components/NewsletterSignUp/index.tsx
+++ src/components/NewsletterSignUp/index.tsx
@@ -12,30 +12,30 @@ export const NewsletterSignUp: React.FC<{
   title = "Transform Your Practice with AI",
   paragraph = "Sign up to receive exclusive AI healthcare updates and unlock your discount.",
   btnText = "Subscribe",
-  width = "640px",
+  width = "680px",
   pb = "70px",
 }) => {
-    return (
-      <section className="relative z-20 overflow-hidden bg-white pb-8 pt-20 dark:bg-dark lg:pb-[50px] lg:pt-[120px]">
-        <div className="container md:px-12">
-          <SectionTitle
-            title={title}
-            paragraph={paragraph}
-            width={width}
-            center
-          />
-          <div className="flex justify-center">
-            <div className={classes.controls}>
-              <TextInput
-                placeholder="Your email"
-                classNames={{ input: classes.input, root: classes.inputWrapper }}
-              />
-              <Button className={`${classes.control} button-primary`}>
-                {btnText}
-              </Button>
-            </div>
+  return (
+    <section className="relative z-20 overflow-hidden bg-white pb-8 pt-20 dark:bg-dark lg:pb-[50px] lg:pt-[120px]">
+      <div className="container md:px-12">
+        <SectionTitle
+          title={title}
+          paragraph={paragraph}
+          width={width}
+          center
+        />
+        <div className="flex justify-center">
+          <div className={classes.controls}>
+            <TextInput
+              placeholder="Your email"
+              classNames={{ input: classes.input, root: classes.inputWrapper }}
+            />
+            <Button className={`${classes.control} button-primary`}>
+              {btnText}
+            </Button>
           </div>
         </div>
-      </section>
-    );
-  };
+      </div>
+    </section>
+  );
+};
