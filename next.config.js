352be569ff209c173640e64bd492abd8b4706471
Will Ramirez <wramirez1980@gmail.com>
2026-02-14 22:11:20 -0600

    chore: update config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+ 
+module.exports = nextConfig
